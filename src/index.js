f3c9e39716deb75cda4a2dba91fc95731aee621f
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-12 12:12:05 +0100

    Feat: Implemented redirection of user after login.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom';
+import firebase from './firebase';
 
 import App from './components/App';
 import Login from './components/Auth/Login';
@@ -9,19 +15,33 @@ import Register from './components/Auth/Register';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-  <Router>
-    <Switch>
-      <Route exact path='/' component={App} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-    </Switch>
-  </Router>
-);
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RootWithAuth = withRouter(Root);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Router>
+      <RootWithAuth />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
