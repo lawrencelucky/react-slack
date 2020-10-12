c0998dddd92b6f0c421c0fd5a08abc57636f9dd9
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-12 09:33:26 +0100

    Feat: Initialised the project, added routing for homepage, login and register, setup firebase.

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+let firebaseConfig = {
+  apiKey: 'AIzaSyC5NDUakiysQEwYPqMT9Opf19Nki15vDOo',
+  authDomain: 'react-slack-clone-c1a7c.firebaseapp.com',
+  databaseURL: 'https://react-slack-clone-c1a7c.firebaseio.com',
+  projectId: 'react-slack-clone-c1a7c',
+  storageBucket: 'react-slack-clone-c1a7c.appspot.com',
+  messagingSenderId: '1040356304045',
+  appId: '1:1040356304045:web:36ddce2b11ff68a451d224',
+  measurementId: 'G-BRNQB21T5P',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
