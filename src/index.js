12ab651971fab2d02ad2cb9b3358af6bd723c5d3
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-12 13:11:15 +0100

    Feat: Integrated redux for global storage and added the spinner component

--- src/index.js
+++ src/index.js
@@ -7,25 +7,40 @@ import {
   withRouter,
 } from 'react-router-dom';
 import firebase from './firebase';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 
+import Spinner from './Spinner';
+
+import rootReducer from './reducers/index';
+import { setUser } from './actions/index';
+
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
+    console.log(this.props.isLoading);
+
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
   }
 
   render() {
-    return (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path='/' component={App} />
         <Route path='/login' component={Login} />
@@ -35,13 +50,19 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading,
+});
+
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <RootWithAuth />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <RootWithAuth />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
