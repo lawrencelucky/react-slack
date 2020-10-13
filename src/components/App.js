7381f5cc2a1f27ac7b7fe2af7c045e5bc14bba67
lawrencelucky <lawrencelucky1999@gmail.com>
2020-10-13 11:13:18 +0100

    Feat: Added the colorPanel user interface, added the messages user interface and it;s functionalities which icludes, storing the messages on firebase, displaying the message submitted.

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,17 @@ import MetaPanel from './MetaPanel/MetaPanel';
 
 import './App.css';
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns='equal' className='app' style={{ background: '#eee' }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -26,6 +30,7 @@ const App = ({ currentUser }) => (
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
 });
 
 export default connect(mapStateToProps)(App);
--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react';
 
 class ColorPanel extends React.Component {
   state = {};
   render() {
-    return <div>ColorPanel</div>;
+    return (
+      <Sidebar
+        as={Menu}
+        icon='labeled'
+        inverted
+        vertical
+        visible
+        width='very thin'
+      >
+        <Divider />
+        <Button icon='add' size='small' color='blue' />
+      </Sidebar>
+    );
   }
 }
 
